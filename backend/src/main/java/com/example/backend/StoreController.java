fb09aa090d55e5a019b79e2ec49e4b1beda6f755
dianabreik <diana.sam341@gmail.com>
2023-10-23 12:15:52 +0200

    Add ExceptionHandler(NoSuchElementException) in StoreController

--- backend/src/main/java/com/example/backend/StoreController.java
+++ backend/src/main/java/com/example/backend/StoreController.java
@@ -1,9 +1,11 @@
 package com.example.backend;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping("/api/products")
@@ -19,5 +21,11 @@ public class StoreController {
     @GetMapping("/{id}")
     public Product getProductDetailsByID(@PathVariable String id){
         return storeService.getProductDetailsByID(id);
+    }
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorMessage handleNoSuchElementException(NoSuchElementException exception){
+        String message = "NoSuchElementException: %s".formatted(exception.getMessage());
+        return new ErrorMessage(message);
     }
 }
