467ad59099bb28bce71744ea382c3dc394491a2d
dianabreik <diana.sam341@gmail.com>
2023-10-24 23:22:54 +0200

    changed the image source in ProductCard to resolve an issue where images were not displayed in the render link in the product gallery

--- frontend/src/ProductCard.tsx
+++ frontend/src/ProductCard.tsx
@@ -1,6 +1,7 @@
 import {Product} from "./Product.ts";
 import './App.css'
 import {Link} from "react-router-dom";
+import bildForCard from '/hintergrund.jpg'
 
 type Props = {
     product: Product
@@ -10,7 +11,7 @@ export default function ProductCard(props: Props) {
     return (
             <div className="productCard">
                 <Link to={`/products/${props.product.id}`}>
-                <img className="productFoto" src={"./src/images/hintergrund.jpg"} alt=""/>
+                <img className="productFoto" src={bildForCard} alt=""/>
                 <div className="productInfoBox">
                   <div className="productName">
                     <p>{props.product.name}</p>
