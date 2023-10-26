38ac9e77acbc4659c159efcc074ac916ac8be7cb
dianabreik <diana.sam341@gmail.com>
2023-10-26 17:58:09 +0200

    change the operation of the functions saveNewProduct & onPriceChange in Frontend

--- frontend/src/AddNewProductPage.tsx
+++ frontend/src/AddNewProductPage.tsx
@@ -5,40 +5,62 @@ import {NewProduct} from "./Types.ts";
 type Props = {
     addNewProductMethod : (newProduct : NewProduct) => void
 }
-export default function AddNewProductPage(props: Props){
+export default function AddNewProductPage(props: Props) {
 
-    const [name,setName] = useState<string>("");
-    const [price,setPrice] = useState<number>(0.0);
+    const [name, setName] = useState<string>("");
+    const [enteredPrice, setEnteredPrice] = useState<string>("");
+    let warningMessageToUser: boolean = false;
     const navigate = useNavigate();
-    function onNameChange(event: ChangeEvent<HTMLInputElement>){
+
+    function onNameChange(event: ChangeEvent<HTMLInputElement>) {
         setName(event.target.value)
     }
-    function onPriceChange(event: ChangeEvent<HTMLInputElement>){
-        const priceValue = Number((event.target.valueAsNumber).toFixed(2));
-        setPrice(priceValue);
+
+    function onPriceChange(event: ChangeEvent<HTMLInputElement>) {
+
+        /*const priceValue = event.target.value;
+        console.log(Number(priceValue))*/
+
+            setEnteredPrice(event.target.value);
+
+        /* const priceValue = (event.target.valueAsNumber);
+         console.log(priceValue)
+         if(!isNaN(priceValue)){
+             setPrice(priceValue);
+         }
+
+ */
     }
-    function saveNewProduct(event:FormEvent<HTMLFormElement>){
+
+    function saveNewProduct(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
+        if(isNaN(Number(enteredPrice.replace(",",".")))){
+           console.error("nicht gültig")
+            return
+        }
+
         const newProductForSave: NewProduct = {
-                name:name,
-                price:price
-            }
+            name: name,
+            price: Number(enteredPrice.replace(",","."))
+        }
         props.addNewProductMethod(newProductForSave);
         setName("");
-        setPrice(0.0);
+        setEnteredPrice("");
         navigate("/products");
-    }
+
+}
 return(
     <>
         <h3>Add a new Product to the store:</h3><br/>
         <form onSubmit={saveNewProduct}>
             <input value={name} onChange={onNameChange} placeholder="Name"/>
             <br/>
-            <input value={price} type="number" onChange={onPriceChange} placeholder="Price"/>
+            <input value={enteredPrice} onChange={onPriceChange} placeholder="Price"/>
             <br/><br/>
             <button>Save</button>
             <button onClick ={() => navigate("/")} type="button">Cancel</button>
         </form>
+        {warningMessageToUser && <span>This Value is NOT a number</span>}
     </>
 )
 }
