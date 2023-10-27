1e17761545eb89b1ebf2157920cb4aa4b3ef0b6d
dianabreik <diana.sam341@gmail.com>
2023-10-27 16:26:06 +0200

    Add responsive navigation bar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,20 +1,32 @@
 import '../App.css'
 import {Link} from "react-router-dom";
+import {useState} from "react";
 export default function Navbar(){
+    const [menuToggel,setManuToggel] = useState<boolean>(false);
+    function toggelMenu(){
+        setManuToggel(!menuToggel);
+    }
     return(
-        <div className={'navbar'}>
+        <>
+        <div className={menuToggel? 'navbar active':'navbar' }>
             <ul className={'nav-menu'}>
                 <li className={'nav-item'}>
-                    <Link to={`/`}>Home</Link> {/*am besten benutzt man da <Link> / <NavLink> oder useNavigate()*/}
+                    <Link to={`/`} className= "navText">Home</Link> {/*am besten benutzt man da <Link> / <NavLink> oder useNavigate()*/}
                 </li>
                 <li className={'nav-item'}>
-                    <Link to={`/products`}>Products</Link>
+                    <Link to={`/products`} className= "navText">Products</Link>
                 </li>
                 <li className={'nav-item'}>
-                    <Link to={`/products/add`}>Add product</Link>
+                    <Link to={`/products/add`} className= "navText">Add product</Link>
                 </li>
             </ul>
         </div>
+            <div onClick={toggelMenu} className="toggle-button">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+        </>
         )
 
 }
