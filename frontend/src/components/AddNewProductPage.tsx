cc8281d47bf8e448ed173785aacb8c6fd055e961
dianabreik <diana.sam341@gmail.com>
2023-11-06 15:52:53 +0100

    Add an input field in the form to add the image URL.

--- frontend/src/components/AddNewProductPage.tsx
+++ frontend/src/components/AddNewProductPage.tsx
@@ -4,26 +4,29 @@ import {NewProduct} from "../Types.ts";
 import '../App.css'
 
 type Props = {
-    addNewProductMethod : (newProduct : NewProduct) => void
+    addNewProductMethod: (newProduct: NewProduct) => void
 }
 export default function AddNewProductPage(props: Props) {
 
     const [name, setName] = useState<string>("");
     const [enteredPrice, setEnteredPrice] = useState<string>("");
+    const [image, setImage] = useState<string>("");
     const [warningMessageToUser, setWarningMessageToUser] = useState(false);
     const navigate = useNavigate();
 
     function onNameChange(event: ChangeEvent<HTMLInputElement>) {
         setName(event.target.value)
     }
-
     function onPriceChange(event: ChangeEvent<HTMLInputElement>) {
-            setEnteredPrice(event.target.value);
+        setEnteredPrice(event.target.value);
+    }
+    function onImageChange(event: ChangeEvent<HTMLInputElement>) {
+        setImage(event.target.value)
     }
 
     function saveNewProduct(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        if(isNaN(Number(Number(enteredPrice.replace(",",".")).toFixed(2)))){
+        if (isNaN(Number(Number(enteredPrice.replace(",", ".")).toFixed(2)))) {
             console.error("This Value is NOT a number");
             setWarningMessageToUser(true);
             return
@@ -31,38 +34,50 @@ export default function AddNewProductPage(props: Props) {
 
         const newProductForSave: NewProduct = {
             name: name,
-            price: Number(Number(enteredPrice.replace(",",".")).toFixed(2))
+            price: Number(Number(enteredPrice.replace(",", ".")).toFixed(2)),
+            imageUrl: image
         }
         props.addNewProductMethod(newProductForSave);
         setName("");
         setEnteredPrice("");
         setWarningMessageToUser(false);
+        setImage("");
         navigate("/products");
 
-}
-return(
-    <div className="addProductPage">
+    }
 
-        <h3 className="titleAddProduct">Add a new Product to the store:</h3><br/>
-        <form onSubmit={saveNewProduct}>
-            <div className="addProductForm">
-            <label htmlFor="fld_Name">Product Name    :</label><br/><input id="fld_Name" value={name} required={true} onChange={onNameChange} placeholder=" Name"/>
-            <br/>
-            <label htmlFor="feld_Price">Product Price   :</label><br/>
-                <div className="input-container">
-                <input id="feld_Price" value={enteredPrice} required={true} onChange={onPriceChange} placeholder=" Price"/>
-                    <span className="input-unit">€</span>
+    return (
+        <div className="addProductPage">
+
+            <h3 className="titleAddProduct">Add a new Product to the store:</h3><br/>
+            <form onSubmit={saveNewProduct}>
+                <div className="addProductForm">
+                    <label htmlFor="fld_Name">Product Name :</label><br/><input id="fld_Name" value={name}
+                                                                                required={true} onChange={onNameChange}
+                                                                                placeholder=" Name"/>
+                    <br/>
+                    <label htmlFor="feld_Price">Product Price :</label><br/>
+                    <div className="input-container">
+                        <input id="feld_Price" value={enteredPrice} required={true} onChange={onPriceChange}
+                               placeholder=" Price"/>
+                        <span className="input-unit">€</span>
+                    </div>
+                    <br/>
+                    {warningMessageToUser && <p>This Value is NOT a number</p>}
+                    <br/>
+                    <label htmlFor="fld_Image">Product Image :</label><br/><input id="fld_Image" value={image}
+                                                                                required={true} onChange={onImageChange}
+                                                                                placeholder=" Image"/>
+                    <br/>
                 </div>
-            <br/>
-            {warningMessageToUser && <p>This Value is NOT a number</p>}
-            <br/>
-            </div>
-            <div className="addProductPageButtons">
-            <button  className="cancelButtonForAddProductPage" onClick ={() => navigate("/")} type="button">Cancel</button>
-            <button className="saveButtonForAddProductPage">Save</button>
-            </div>
-        </form>
+                <div className="addProductPageButtons">
+                    <button className="cancelButtonForAddProductPage" onClick={() => navigate("/")}
+                            type="button">Cancel
+                    </button>
+                    <button className="saveButtonForAddProductPage">Save</button>
+                </div>
+            </form>
 
-    </div>
-)
+        </div>
+    )
 }
