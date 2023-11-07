2f1c457a08d9b002392fddc88881f8ee33afeb38
dianabreik <diana.sam341@gmail.com>
2023-11-07 12:37:29 +0100

    modifying the save method in the StoreService

--- backend/src/main/java/com/example/backend/StoreService.java
+++ backend/src/main/java/com/example/backend/StoreService.java
@@ -30,7 +30,7 @@ public class StoreService {
     }
 
     public Product saveNewProduct(NewProduct newProduct) {
-        Product product = new Product(UUID.randomUUID().toString(), newProduct.name(), newProduct.price(), newProduct.imageUrl());
+        Product product = new Product(UUID.randomUUID().toString(), newProduct.name(), newProduct.price(), newProduct.imageUrl(),newProduct.description(),newProduct.category());
         return storeRepository.save(product);
     }
 
