e4e4df6a25c60bb36a308a441adedbe2f41a69b4
dianabreik <diana.sam341@gmail.com>
2023-11-08 18:46:56 +0100

    Add a group of options in the nav-bar with select + add a method that makes the appropriate call to a recursive method depending on the selection that was clicked.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,27 @@
 import '../App.css'
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {useState} from "react";
-export default function Navbar(){
+import {ProductCategory} from "../Types.ts";
+
+type Props = {
+    findProductsByCategory: (category:ProductCategory)=> void
+}
+export default function Navbar(props:Props){
     const [menuToggel,setMenuToggel] = useState<boolean>(false);
+    const navigate = useNavigate();
     function toggelMenu(){
         setMenuToggel(!menuToggel);
+    }
+    function navigateWhenClickAllProducts(option: string){
+        if(option==="ALL")
+           navigate("/products");
+        else if(option==="LAPTOPS")
+            props.findProductsByCategory("LAPTOPS")
+        else if(option==="SMARTPHONES")
+            props.findProductsByCategory("SMARTPHONES")
+        else
+            props.findProductsByCategory("SMARTWATCHES")
+
     }
     return(
         <>
@@ -14,7 +31,12 @@ export default function Navbar(){
                     <Link to={`/`} className= "navText">Home</Link> {/*am besten benutzt man da <Link> / <NavLink> oder useNavigate()*/}
                 </li>
                 <li className={'nav-item'}>
-                    <Link to={`/products`} className= "navText">Products</Link>
+                        <select onChange={e =>navigateWhenClickAllProducts (e.target.value)}>
+                            <option  value="ALL" className= "navText">All Products</option>
+                            <option  value="LAPTOPS" className= "navText">Laptops</option>
+                            <option  value="SMARTPHONES" className= "navText">Smartphones</option>
+                            <option  value="SMARTWATCHES" className= "navText">Smartwatches</option>
+                        </select>
                 </li>
                 <li className={'nav-item'}>
                     <Link to={`/products/add`} className= "navText">Add product</Link>
