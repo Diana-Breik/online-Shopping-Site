804d568667eec96d0815376652688111a5f4c414
dianabreik <diana.sam341@gmail.com>
2023-11-08 12:14:10 +0100

    Create a function definition in the repository to get all products from a specific category

--- backend/src/main/java/com/example/backend/StoreRepository.java
+++ backend/src/main/java/com/example/backend/StoreRepository.java
@@ -1,10 +1,14 @@
 package com.example.backend;
 
 import com.example.backend.models.Product;
+import com.example.backend.models.ProductCategory;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
-public interface StoreRepository extends MongoRepository<Product,String> {
+public interface StoreRepository extends MongoRepository<Product, String> {
+    List<Product> findAllByCategory(ProductCategory category);
 }
