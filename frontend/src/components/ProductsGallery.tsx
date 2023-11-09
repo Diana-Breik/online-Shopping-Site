3857f23f6c2ac2863866f9bd7c92ffb889b51e1a
dianabreik <diana.sam341@gmail.com>
2023-11-09 11:38:42 +0100

    Change the location of both the filtering method and category selection from component navbar to ProductsGallery

--- frontend/src/components/ProductsGallery.tsx
+++ frontend/src/components/ProductsGallery.tsx
@@ -2,6 +2,7 @@ import {Product, ProductCategory} from "../Types.ts";
 import ProductCard from "./ProductCard.tsx";
 import Navbar from "./Navbar.tsx";
 import '../App.css'
+import {useNavigate} from "react-router-dom";
 
 type Props = {
     products: Product[]
@@ -10,12 +11,29 @@ type Props = {
 }
 export default function ProductsGallery(props: Props){
 
+    const navigate = useNavigate();
+    function navigateWhenClickAllProducts(option: string){
+        if(option==="ALL")
+            navigate("/products");
+        else if(option==="LAPTOPS")
+            props.findProductsByCategory("LAPTOPS")
+        else if(option==="SMARTPHONES")
+            props.findProductsByCategory("SMARTPHONES")
+        else
+            props.findProductsByCategory("SMARTWATCHES")
+
+    }
     return (
            <div className="productsGalleryBackGround">
              <div className="topBar">
-                <Navbar findProductsByCategory={props.findProductsByCategory} />
+                <Navbar />
              </div>
-             <h2 className="titleProductsGallery">All products</h2>
+               <select className="filterProductsInGallery" onChange={e =>navigateWhenClickAllProducts (e.target.value)}>
+                   <option  value="ALL" className= "navText">All Products</option>
+                   <option  value="LAPTOPS" className= "navText">Laptops</option>
+                   <option  value="SMARTPHONES" className= "navText">Smartphones</option>
+                   <option  value="SMARTWATCHES" className= "navText">Smartwatches</option>
+               </select>
              <div className="productsGallery">
                 {
                     props.products.map( product =>
