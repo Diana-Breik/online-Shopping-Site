08b4ec0842970ccab3b416987d9e49b8c67c2fff
dianabreik <diana.sam341@gmail.com>
2023-11-10 07:43:44 +0100

    Add props to the nav-bar component contains a method that sets the select value as (all products) when clicking on products in the nav-bar.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,32 +2,38 @@ import '../App.css'
 import {Link} from "react-router-dom";
 import {useState} from "react";
 
-export default function Navbar(){
-    const [menuToggel,setMenuToggel] = useState<boolean>(false);
-    function toggelMenu(){
+type Props = {
+    handleClick?: () => void
+}
+export default function Navbar(props: Props) {
+    const [menuToggel, setMenuToggel] = useState<boolean>(false);
+
+    function toggelMenu() {
         setMenuToggel(!menuToggel);
     }
-    return(
+
+    return (
         <>
-        <div className={menuToggel? 'navbar active':'navbar' }>
-            <ul className={'nav-menu'}>
-                <li className={'nav-item'}>
-                    <Link to={`/`} className= "navText">Home</Link> {/*am besten benutzt man da <Link> / <NavLink> oder useNavigate()*/}
-                </li>
-                <li className={'nav-item'}>
-                    <Link to={`/products`} className= "navText">Products</Link>
-                </li>
-                <li className={'nav-item'}>
-                    <Link to={`/products/add`} className= "navText">Add product</Link>
-                </li>
-            </ul>
-        </div>
+            <div className={menuToggel ? 'navbar active' : 'navbar'}>
+                <ul className={'nav-menu'}>
+                    <li className={'nav-item'}>
+                        <Link to={`/`}
+                              className="navText">Home</Link> {/*am besten benutzt man da <Link> / <NavLink> oder useNavigate()*/}
+                    </li>
+                    <li className={'nav-item'}>
+                        <Link to={`/products`} className="navText" onClick={props.handleClick}>Products</Link>
+                    </li>
+                    <li className={'nav-item'}>
+                        <Link to={`/products/add`} className="navText">Add product</Link>
+                    </li>
+                </ul>
+            </div>
             <button type={"button"} onClick={toggelMenu} className="toggle-button">
                 <span className="bar"></span>
                 <span className="bar"></span>
                 <span className="bar"></span>
             </button>
         </>
-        )
+    )
 
 }
