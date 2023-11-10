bf46b80c6d9ac25ff25e396248cad531bd1aefc2
dianabreik <diana.sam341@gmail.com>
2023-11-10 07:56:51 +0100

    Merging both the filter func + the map func to display the products directly, without using an additional list to save the results after the filter.

--- frontend/src/components/ProductsGallery.tsx
+++ frontend/src/components/ProductsGallery.tsx
@@ -1,4 +1,4 @@
-import {Product, ProductCategory} from "../Types.ts";
+import {Product} from "../Types.ts";
 import ProductCard from "./ProductCard.tsx";
 import Navbar from "./Navbar.tsx";
 import '../App.css'
@@ -9,26 +9,18 @@ type Props = {
     deleteProductMethod: (id : string)=> void
 }
 export default function ProductsGallery(props: Props){
-    const [optionValue, setOptionValue] = useState<string>("");
-    const [productsByCategory, setProductsByCategory] = useState<Product[]>([]);
+    const [optionValue, setOptionValue] = useState<string>("ALL");
     function findProductsByCategory(option: string){
-        setOptionValue(option);
-        if(option==="ALL")
-        {
-           setProductsByCategory(props.products);
-        }
-        else
-        {
-            const category= (option as ProductCategory);
-            const filteredProducts = props.products.filter(product => product.category === category);
-            setProductsByCategory(filteredProducts);
-        }
+        setOptionValue(option)
+    }
 
+    function resetOptionValueToALL() {
+        setOptionValue("ALL")
     }
     return (
            <div className="productsGalleryBackGround">
              <div className="topBar">
-                <Navbar />
+                <Navbar handleClick={resetOptionValueToALL}/>
              </div>
                <select value={optionValue} className="filterProductsInGallery" onChange={e =>findProductsByCategory (e.target.value)}>
                    <option  value="ALL" className= "navText">All Products</option>
@@ -38,16 +30,9 @@ export default function ProductsGallery(props: Props){
                </select>
              <div className="productsGallery">
                 {
-                    optionValue ==="" ? (
-                    props.products.map( product =>
+                    props.products.filter(product => product.category === optionValue || optionValue === "ALL").map( product =>
                         <ProductCard key={product.id} product={product} deleteProductMethod={props.deleteProductMethod}/>
                     )
-                    ):(
-                        productsByCategory.map(product =>
-                            <ProductCard key={product.id} product={product} deleteProductMethod={props.deleteProductMethod}/>
-                        )
-                    )
-
                 }
              </div>
            </div>
